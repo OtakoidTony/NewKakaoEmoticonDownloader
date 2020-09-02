10d5a665e2faeab197573369269fc6299fb0c374
박현 <36785562+OtakoidTony@users.noreply.github.com>
2020-09-02 14:45:50 +0900

    upload

---
+++ NewKakaoEmoticonDownloader/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace NewKakaoEmoticonDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.titleColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.artistColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.encodedColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.titleColumnHeader,
+            this.artistColumnHeader,
+            this.encodedColumnHeader});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(11, 42);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(543, 355);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // titleColumnHeader
+            // 
+            this.titleColumnHeader.Text = "이모티콘 타이틀";
+            this.titleColumnHeader.Width = 232;
+            // 
+            // artistColumnHeader
+            // 
+            this.artistColumnHeader.Text = "제작자";
+            this.artistColumnHeader.Width = 120;
+            // 
+            // encodedColumnHeader
+            // 
+            this.encodedColumnHeader.Text = "아이템 코드";
+            this.encodedColumnHeader.Width = 80;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 12);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(453, 14);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(103, 25);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "검색";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(562, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(228, 250);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(562, 316);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(229, 31);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(562, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 45);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kakao Emoticon Downloader v2.0\r\n\r\nAuthor » Rojiku\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(562, 271);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 38);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "다운로드";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 412);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximumSize = new System.Drawing.Size(820, 459);
+            this.MinimumSize = new System.Drawing.Size(820, 459);
+            this.Name = "Form1";
+            this.Text = "Kakao Emoticon Downloader v2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader titleColumnHeader;
+        private System.Windows.Forms.ColumnHeader artistColumnHeader;
+        private System.Windows.Forms.ColumnHeader encodedColumnHeader;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ NewKakaoEmoticonDownloader/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+
+namespace NewKakaoEmoticonDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+        public JToken searchResult;
+        public int selectedIndex = -1;
+
+        private void whenSelect()
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                ListView.SelectedListViewItemCollection items = listView1.SelectedItems;
+                ListViewItem lvItem = items[0];
+                selectedIndex = listView1.Items.IndexOf(lvItem);
+                pictureBox1.Load(searchResult[selectedIndex]["titleDetailUrl"].ToString());
+            }
+        }
+        
+        private void printListView()
+        {
+            searchResult = KakaoEmoticon.Search(textBox1.Text)["items"];
+            foreach (var i in searchResult)
+            {
+                String[] aa = { (String)i["title"], (String)i["artist"], (String)i["encoded"] };
+                ListViewItem newitem = new ListViewItem(aa);
+                listView1.Items.Add(newitem);
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            whenSelect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            printListView();
+        }
+
+
+
+        private void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                whenSelect();
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                printListView();
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool DownloadRemoteImageFile(string uri, string fileName)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                bool bImage = response.ContentType.StartsWith("image",
+                    StringComparison.OrdinalIgnoreCase);
+                if ((response.StatusCode == HttpStatusCode.OK ||
+                    response.StatusCode == HttpStatusCode.Moved ||
+                    response.StatusCode == HttpStatusCode.Redirect) &&
+                    bImage)
+                {
+                    using (Stream inputStream = response.GetResponseStream())
+                    using (Stream outputStream = File.OpenWrite(fileName))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        do
+                        {
+                            bytesRead = inputStream.Read(buffer, 0, buffer.Length);
+                            outputStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead != 0);
+                    }
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    ListView.SelectedListViewItemCollection items = listView1.SelectedItems;
+                    ListViewItem lvItem = items[0];
+                    string title = lvItem.SubItems[0].Text;
+                    string itemcode = lvItem.SubItems[2].Text;
+                    string folderPath = Application.StartupPath + "\\" + title;
+
+                    string[] result = KakaoEmoticon.GetThumbUrl(itemcode);
+                    int size = result.Length;
+                    DirectoryInfo di = new DirectoryInfo(folderPath);
+                    if (di.Exists == false)
+                    {
+                        di.Create();
+                    }
+                    progressBar1.Style = ProgressBarStyle.Continuous;
+                    progressBar1.Minimum = 0;
+                    progressBar1.Maximum = size;
+                    progressBar1.Step = 1;
+                    progressBar1.Value = 0;
+                    for (int i = 0; i < size; i++)
+                    {
+                        webClient.DownloadFile(result[i], folderPath + "\\" + (i+".png"));
+                        progressBar1.PerformStep();
+                    }
+                }
+            }
+        }
+    }
+    public class KakaoEmoticon
+    {
+        public static JObject Search(string searchText)
+        {
+            var client = new RestClient("https://e.kakao.com/search?q=" + searchText);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            var response = client.Execute(request);
+            var httpDoc = new HtmlAgilityPack.HtmlDocument();
+            httpDoc.LoadHtml(response.Content);
+            var nodeCol = httpDoc.DocumentNode.SelectNodes("//*[@id=\"mArticle\"]/div[2]");
+            var searchResult = nodeCol[0].Attributes["data-react-props"].Value;
+            return JObject.Parse(searchResult.Replace("&quot;", "\""));
+        }
+
+        public static JObject Info(string titleUrl)
+        {
+            var client = new RestClient("https://e.kakao.com/t/" + titleUrl);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("DNT", "1");
+            request.AddHeader("Upgrade-Insecure-Requests", "1");
+            request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            IRestResponse response = client.Execute(request);
+            var httpDoc = new HtmlAgilityPack.HtmlDocument();
+            httpDoc.LoadHtml(response.Content);
+            var nodeCol = httpDoc.DocumentNode.SelectNodes("//*[@id=\"dkWrap\"]/div[2]");
+            return JObject.Parse(nodeCol[0].Attributes["data-react-props"].Value.Replace("&quot;", "\""));
+        }
+
+        public static string[] GetThumbUrl(string itemCode)
+        {
+            var client = new RestClient("https://e.kakao.com/detail/thumb_url?item_code=" + itemCode);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            JObject res = JObject.Parse(response.Content);
+            return JArray.FromObject(res["body"]).ToObject<string[]>();
+        }
+    }
+}
